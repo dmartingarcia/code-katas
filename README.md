222835652d5783beee872274df36a106f70cae2f
David Martin Garcia <davidmartingarcia0@gmail.com>
2021-05-04 20:47:26 +0200

    initial commit, adding README.md that indicates the details, plus a code snippet that checks the result

---
+++ README.md
@@ -0,0 +1,7 @@
+Having an integer in range [-8000, 8000]
+
+Obtain the maximum value of the operation of adding a 5.
+I.ex:
+
+  Having 145, maximum value is 5145.
+  Having 643, maximum value is 6543.
---
+++ code.rb
@@ -0,0 +1,9 @@
+def solution(x)
+end
+
+raise if solution(234) != 5234
+raise if solution(-234) != -5234
+raise if solution(671) != 6751
+raise if solution(-671) != -5671
+raise if solution(-1) != -15
+raise if solution(1) != 51
