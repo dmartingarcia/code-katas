ee2f8874a66219af7b22d10425e99b4ddc834478
David Martin Garcia <davidmartingarcia0@gmail.com>
2021-05-04 23:11:56 +0200

    adding recursive solution

--- code.rb
+++ code.rb
@@ -16,17 +16,35 @@ class FirstSolution
   end
 end
 
+class SecondSolution
+  def self.solution(x)
+    is_negative = x < 0
+    integer_array = Tools.split_integer(x)
+
+    recursive_solution(0, integer_array, is_negative)
+  end
+
+  def self.recursive_solution(head, tail, is_negative)
+    return -9999 if tail.is_a?(NilClass)
+    value = [head, 5, tail].flatten.join("").to_i
+
+    value = -value if is_negative
+
+    [value, recursive_solution("#{head}#{tail[0]}", tail[1..-1], is_negative)].max
+  end
+end
+
 class Tools
   def self.split_integer(x)
-    x.to_s.split("").map{|val| val == "-" ? nil : val.to_i }.compact
+    x.to_s.split("").map{|val| val == "-" ? nil : val }.compact
   end
 
   def self.check_solution(value, expected_value)
     puts "VALUE: #{value} EXPECTED: #{expected_value}"
     raise if FirstSolution.solution(value) != expected_value
+    raise if SecondSolution.solution(value) != expected_value
   end
 end
-
 
 raise if Tools.check_solution(234, 5234)
 raise if Tools.check_solution(-234, -2345)
