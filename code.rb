aedbf7840871b33d507dc00f5c77839df03dcee9
David Martin Garcia <davidmartingarcia0@gmail.com>
2021-05-04 23:00:31 +0200

    adapting code to handle multiple solutions

--- code.rb
+++ code.rb
@@ -1,27 +1,37 @@
-def solution(x)
-  is_negative = x < 0
-  integer_array = split_integer(x)
-  result = -9999
+class FirstSolution
+  def self.solution(x)
+    is_negative = x < 0
+    integer_array = Tools.split_integer(x)
+    result = -9999
 
-  (integer_array.size + 1).times.each do |index|
-    value = integer_array.dup.insert(index, 5).join("").to_i
+    (integer_array.size + 1).times.each do |index|
+      value = integer_array.dup.insert(index, 5).join("").to_i
 
-    value = -value if is_negative
+      value = -value if is_negative
 
-    result = [value, result].max
+      result = [value, result].max
+    end
+
+    result
+  end
+end
+
+class Tools
+  def self.split_integer(x)
+    x.to_s.split("").map{|val| val == "-" ? nil : val.to_i }.compact
   end
 
-  result
+  def self.check_solution(value, expected_value)
+    puts "VALUE: #{value} EXPECTED: #{expected_value}"
+    raise if FirstSolution.solution(value) != expected_value
+  end
 end
 
-def split_integer(x)
-  x.to_s.split("").map{|val| val == "-" ? nil : val.to_i }.compact
-end
 
-raise if solution(234) != 5234
-raise if solution(-234) != -2345
-raise if solution(671) != 6751
-raise if solution(-671) != -5671
-raise if solution(-1) != -15
-raise if solution(1) != 51
+raise if Tools.check_solution(234, 5234)
+raise if Tools.check_solution(-234, -2345)
+raise if Tools.check_solution(671, 6751)
+raise if Tools.check_solution(-671, -5671)
+raise if Tools.check_solution(-1, -15)
+raise if Tools.check_solution(1, 51)
 puts "Allright!!!"
