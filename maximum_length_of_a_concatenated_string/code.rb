9aae1379c4965e2def44b662618306ae4a017870
David Martin <11503528+dmartingarcia@users.noreply.github.com>
2025-02-26 12:15:50 +0100

    fix maximum lenght of a concatenated string

--- maximum_length_of_a_concatenated_string/code.rb
+++ maximum_length_of_a_concatenated_string/code.rb
@@ -1,30 +1,34 @@
 class Solver
-  def max_length(arr)
+  def self.max_length(arr)
     head, *tail = arr
-    
     return max_length_r(head.length, head, tail)
   end
 
-  def max_length_r(max_val, head, tail)
+  def self.max_length_r(max_val, head, tail)
     return max_val if tail.size == 0
-    
+
     tail.each do |elem|
-      max_val = [max_val, [head, elem].join.length].max 
+      next if (head.chars & elem.chars).any?
+      partial = [head, elem].join
+      puts partial
+      max_val = [max_val, partial.length].max
     end
-    
+
     head, *tail = tail
-    
-    return max_length_r(max_val, head, tail) 
+
+    return max_length_r(max_val, head, tail)
   end
 end
 
 class Tools
   def self.check_solution(value, expected_value)
-    raise if Solver.solution(value) != expected_value
+    result = Solver.max_length(value)
+    raise "invalid #{result} != #{expected_value}" if result != expected_value
   end
 end
 
-raise if Tools.check_solution(['un','iq''ue'], 4)
+raise if Tools.check_solution(['un','iq','ue'], 4)
 raise if Tools.check_solution(['cha','r','act','ers'], 6)
 raise if Tools.check_solution(['abcdefghijklmnopqrstuvwxyz'], 26)
 
+puts "all tests passed"
